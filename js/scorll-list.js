2729c05cae47b58e303cd8ac2a029e22e194a1f6
monica <tangerinetan@126.com>
2015-01-29 17:15:03 +0800

    index

---
+++ js/scorll-list.js
@@ -0,0 +1,27 @@
+
+$(function(){
+	var $leftScoll = $('.scorll-left');
+	var $rightScoll = $('.scorll-right');
+	scorllFirst($leftScoll);
+	scorllFirst($rightScoll);
+});
+
+function scorllFirst(obj2){
+	var $this = $(obj2);
+	var scrollTimer;
+	$this.hover(function(){
+		clearInterval(scrollTimer);
+	},function(){
+	scrollTimer = setInterval(function(){
+	scrollNews($this);
+	}, 2000 );
+	}).trigger("mouseout");
+};
+function scrollNews(obj){
+	var $self = obj.find("ul:first");
+	var lineHeight = $self.find("li:first").height();
+	$self.animate({ "margin-top" : -lineHeight +"px" },600 ,
+	function(){
+		$self.css({"margin-top":"0px"}).find("li:first").appendTo($self);
+	})
+}
